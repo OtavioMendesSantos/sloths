1a5a66d6a790052f4dfdabe40f2bbea02616a5e0
Otavio Mendes Santos <otaviomendessantos2019@gmail.com>
2025-05-10 22:09:53 -0300

    :sparkles: feat: lendo .env

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
+<?php
+include 'src/config/index.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sloths</title>
 </head>
+
 <body>
     <h1>Sloths</h1>
 
 </body>
+
 </html>
